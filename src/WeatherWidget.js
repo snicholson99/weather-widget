97565345b73cdecbdbeb7a49ddafa4ea71b78338
SNicholson99 <scott.nicholson@live.co.uk>
2019-08-11 13:54:21 +0100

    obtained current location

--- src/WeatherWidget.js
+++ src/WeatherWidget.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './WeatherWidget.css';
 
-const WeatherWidget = () => {
-  return (
-    <div className="weather-widget">
-      <h1>Weather Widget Setup</h1>
-    </div>
-  );
+class WeatherWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: "",
+      longitude: "",
+    }
+  }
+
+  componentDidMount() {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(
+        this.onGetLocSuccess, this.onGetLocError
+      );
+    }
+  }
+
+  onGetLocSuccess = (pos) => {
+    const { latitude, longitude } = pos.coords;
+    this.setState({ latitude, longitude })
+  }
+
+  onGetLocError = (err) => {
+    console.error("Failed to getCurrentPosition", err);
+  }
+
+  render() {
+    const { latitude, longitude } = this.state;
+    return (
+      <div className="weather-widget">
+        <h1>Weather Widget</h1>
+        <p>Latitude: {latitude}</p>
+        <p>Longitude: {longitude}</p>
+      </div>
+    );
+  }
 }
 
 export default WeatherWidget;
