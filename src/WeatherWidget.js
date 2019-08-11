7ba86ae6414d72e94a1f692e8060427dba309617
SNicholson99 <scott.nicholson@live.co.uk>
2019-08-11 12:52:02 +0100

    setting up application

---
+++ src/WeatherWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeatherWidget.css';
+
+const WeatherWidget = () => {
+  return (
+    <div className="weather-widget">
+      <h1>Weather Widget Setup</h1>
+    </div>
+  );
+}
+
+export default WeatherWidget;
